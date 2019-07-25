4bf0fe7f720dce4a0783fcd8d47a05ea93335a26
Daria Ratyńska <ratynskadaria@gmail.com>
2019-07-25 14:18:04 +0200

    Created starting react application

---
+++ portfolioapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        IT'S ME! DARIA!
+      </header>
+    </div>
+  );
+}
+
+export default App;
